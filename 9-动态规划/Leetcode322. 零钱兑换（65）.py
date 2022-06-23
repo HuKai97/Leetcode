b582e2c2961f0c8d517686c9852d11e3d0ddfbe9
SCAU-HuKai <1059502393@qq.com>
2022-06-23 11:26:28 +0800

    动态规划第二遍完成！

--- 9-动态规划/Leetcode322. 零钱兑换（65）.py
+++ 9-动态规划/Leetcode322. 零钱兑换（65）.py
@@ -12,3 +12,29 @@ class Solution:
             for j in range(coins[i], amount+1):
                 dp[j] = min(dp[j], dp[j-coins[i]]+1)
         return dp[-1] if dp[-1] != float('inf') else -1
+
+
+# Leetcode518. 零钱兑换 II
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 0 or len(coins) == 0: return 1
+        dp = [0 for _ in range(amount+1)]
+        dp[0] = 1
+        for i in range(len(coins)):
+            for j in range(coins[i], amount+1):
+                if j >= coins[i]:
+                    dp[j] += dp[j-coins[i]]
+        return dp[-1]
+
+# Leetcode139.单词拆分
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if len(s) == 0 or len(wordDict) == 0: return False
+        # dp[j]表示从i位置到j位置的子串能否被切割成字典中的单词
+        dp = [False for _ in range(len(s)+1)]
+        dp[0] = True
+        for i in range(len(s)):
+            for j in range(i+1, len(s)+1):
+                if dp[i]==True and s[i:j] in wordDict:
+                    dp[j] = True
+        return dp[-1]
